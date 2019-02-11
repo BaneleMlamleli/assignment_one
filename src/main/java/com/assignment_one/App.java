27d4ecff010f3faf8685b80e1c70e01e2ea2251b
BaneleMlamleli <mlamlelibanele@yahoo.com>
2019-02-12 00:23:54 +0200

    Added all the input validations

--- src/main/java/com/assignment_one/App.java
+++ src/main/java/com/assignment_one/App.java
@@ -14,27 +14,46 @@ public class App
         Scanner scn = new Scanner(System.in);
         System.out.println("\n\t********************************************\n" +
                 "\t*  WELCOME TO MY SIMPLE CALCULATOR PROGRAM *\n"+
-                "\t********************************************");
+                "\t********************************************\n");
 
-        char exit;
+        char exit, option;
         do {
             System.out.print("Choose option below\n"+
                     "A - Addition\n"+
                     "S - Subtraction\n"+
                     "M - Multiplication\n"+
-                    "D - Division\n\n"+
+                    "D - Division\n"+
                     "Option:  ");
 
-            char option = scn.next().toLowerCase().charAt(0);
+            option = scn.next().toLowerCase().charAt(0);
+
+            //checking if the entered option matches the options provided
+            while((option != 'a')&&(option != 's')&&(option != 'm')&&(option != 'd')){
+                JOptionPane.showMessageDialog(null, "Incorrect entry. re-try", "Error!", JOptionPane.ERROR_MESSAGE);
+                System.out.print("Choose option below\n"+
+                        "A - Addition\n"+
+                        "S - Subtraction\n"+
+                        "M - Multiplication\n"+
+                        "D - Division\n"+
+                        "Option:  ");
+                option = scn.next().toLowerCase().charAt(0);
+            }
 
             switch (option){
-                case 'a': ;break;
+                case 'a': System.out.println("\n\tAddition\n\t********\n");break;
                 case 's': ;break;
                 case 'm': ;break;
                 case 'd': ;break;
             }
-            System.out.print("Exit calculator Yes/No: ");
+            System.out.print("\n**********************\nExit calculator Yes/No: ");
             exit = scn.next().toLowerCase().charAt(0);
+
+            //checking if the entered option matches the options provided
+            while((exit != 'y')&&(exit != 'n')){
+                JOptionPane.showMessageDialog(null, "Incorrect entry. re-try", "Error!", JOptionPane.ERROR_MESSAGE);
+                System.out.println("Enter Yes/No: ");
+                exit = scn.next().toLowerCase().charAt(0);
+            }
         }while (exit != 'y');
 
         System.out.println("\n\t******************************************\n" +
